7e898f1de8c68f19693060ab183d5b23f86198ce
Zhao Kun <igozhaokun@163.com>
2018-12-04 22:49:14 +0800

    handle read file encoding error

--- src/utils/data_mining/fenci_fixed_data.py
+++ src/utils/data_mining/fenci_fixed_data.py
@@ -4,21 +4,24 @@
 #	- pip install jieba
 #	- pip install scikit-learn
 
+import os
+import random
+from collections import Counter
+import numpy as np
+
 import jieba
 import jieba.analyse
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
-import os
-import random
-from collections import Counter
-import numpy as np
 from sklearn import metrics
 from sklearn import svm
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
+
 
 CONTENT_TYPE = {
     'yl': '娱乐',
@@ -33,6 +36,20 @@ CONTENT_TYPE = {
     'wh':'文化'
 }
 
+def readFile(new_foler_path, file):
+    '''
+    读取文件内容
+    '''
+    file_path = os.path.join(new_foler_path, file)
+    
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read()
+    except Exception as e:
+        print('Read {0} error {1}'.format(file_path, str(e.message)))
+
+    return None
+
 """
     Parameters:
     folder_path - 文本存放路径
@@ -45,7 +62,6 @@ CONTENT_TYPE = {
     all_words_list
     全部样本 - 分词（筛选，关键词-类别）- 向量化 - 训练集、测试集分开 - 训练 - 评估
 """
-
 def TextProcessing(folder_path, key_words_path, test_size = 0.5):
     class_list = []
     all_word_list = []
@@ -64,32 +80,40 @@ def TextProcessing(folder_path, key_words_path, test_size = 0.5):
         if os.path.isdir(os.path.join(folder_path,folder)):
             print("------&&&&-----")
             print(folder)
+
             new_foler_path = os.path.join(folder_path,folder)
             files = os.listdir(new_foler_path)
             count = 1
-            for file in files:
-                with open(os.path.join(new_foler_path,file),'r',encoding='utf-8') as f:
-                    content = f.read()
-                    jieba.analyse.set_stop_words(key_words_path)
-                    #筛选名词、人名、地名、机构团体名、动词
-                    content_cut = jieba.analyse.extract_tags(content, topK = 30, allowPOS = {'n', 'nr', 'ns', 'nt', 'v'})
-                    #content_cut = jieba.analyse.extract_tags(content, topK = 30)
-                    if count < len(files)/2:
-                        train_word_list.append(Convert2Str(content_cut))
-                        train_class_list.append(CONTENT_TYPE[folder])
-                    else:
-                        test_word_list.append(Convert2Str(content_cut))
-                        test_class_list.append(CONTENT_TYPE[folder])
-                    count = count + 1
-                    all_word_list.append(Convert2Str(content_cut))
-                    class_list.append(CONTENT_TYPE[folder])
-                    
-                    #write the top_k_word into document
             
-                    """with open(os.path.join(key_words_path, key_words_txt), 'a', encoding = 'utf-8') as top_k_word:
-                        top_k_word.write(str(content_cut))
-                        top_k_word.write("---***分割线***---")
-                    top_k_word.write("\n")"""
+            for file in files:
+
+                # 读取文件
+                content = readFile(new_foler_path, file)
+                if content is None:
+                    continue
+
+                jieba.analyse.set_stop_words(key_words_path)
+
+                #筛选名词、人名、地名、机构团体名、动词
+                content_cut = jieba.analyse.extract_tags(content, topK = 30, allowPOS = {'n', 'nr', 'ns', 'nt', 'v'})
+                #content_cut = jieba.analyse.extract_tags(content, topK = 30)
+                if count < len(files)/2:
+                    train_word_list.append(Convert2Str(content_cut))
+                    train_class_list.append(CONTENT_TYPE[folder])
+                else:
+                    test_word_list.append(Convert2Str(content_cut))
+                    test_class_list.append(CONTENT_TYPE[folder])
+
+                count = count + 1
+                all_word_list.append(Convert2Str(content_cut))
+                class_list.append(CONTENT_TYPE[folder])
+                    
+                #write the top_k_word into document
+            
+                """with open(os.path.join(key_words_path, key_words_txt), 'a', encoding = 'utf-8') as top_k_word:
+                top_k_word.write(str(content_cut))
+                top_k_word.write("---***分割线***---")
+                top_k_word.write("\n")"""
 
     return all_word_list, train_word_list, train_class_list, test_word_list, test_class_list
 
