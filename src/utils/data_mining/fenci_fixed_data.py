c494e7f219859b6a6b640f7bbd18b83885922882
lei zhao <lucy@leis-MacBook-Pro.local>
2018-12-16 23:48:30 +0800

    add cross-validation method and remove type verb in jieba cut

--- src/utils/data_mining/fenci_fixed_data.py
+++ src/utils/data_mining/fenci_fixed_data.py
@@ -12,16 +12,18 @@ import numpy as np
 import jieba
 import jieba.analyse
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from sklearn import svm
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
 
 
 CONTENT_TYPE = {
@@ -122,8 +124,8 @@ def TextProcessing(folder_path, stop_words_path, user_dict_path, key_words_path,
                 jieba.suggest_freq(("平均", "收入"), tune = True)		
 
 
-                #筛选名词、机构团体名、动词
-                content_cut = jieba.analyse.extract_tags(content, topK = 30, allowPOS = {'n', 'nt', 'v'})
+                #筛选名词、机构团体名
+                content_cut = jieba.analyse.extract_tags(content, topK = 30, allowPOS = {'n', 'nt'})
                
                 if count < maxLen * test_size:
                     train_word_list.append(Convert2Str(content_cut))
@@ -139,12 +141,12 @@ def TextProcessing(folder_path, stop_words_path, user_dict_path, key_words_path,
                     
                 #write the top_k_word into document
             
-                with open(key_words_path, 'a', encoding='utf-8') as top_k_word:
+                '''with open(key_words_path, 'a', encoding='utf-8') as top_k_word:
                     top_k_word.write(new_foler_path + '/' + file + '\n')
                     top_k_word.write(str(content_cut))
                     top_k_word.write("\n---***分割线***---")
                     top_k_word.write("\n")
-
+'''
     return all_word_list, train_word_list, train_class_list, test_word_list, test_class_list
 
 #将逗号分隔的中文改为用空格分隔连接的字符串
@@ -155,12 +157,25 @@ def Convert2Str(iter):
         target_str = target_str + ' '
     return  target_str
 
+#cross-validation
+def svm_cross_validation(train_x, train_y):
+    model = SVC(kernel='rbf', probability=True)
+    param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma': [0.001, 0.0001]}
+    grid_search = GridSearchCV(model, param_grid, n_jobs = 8, verbose=1)
+    grid_search.fit(train_x, train_y)
+    best_parameters = grid_search.best_estimator_.get_params()
+    for para, val in list(best_parameters.items()):
+        print(para, val)
+    model = SVC(kernel='rbf', C=best_parameters['C'], gamma=best_parameters['gamma'], probability=True)
+    model.fit(train_x, train_y)
+    return model
+
 if __name__ == '__main__':
     all_word_list, train_word_list, train_class_list, test_word_list, test_class_list = TextProcessing('./data/source-data','./stop_words.txt', 'user_dict.txt', \
 			'./key_words.txt', \
 			test_size = 0.5)
     
-    #贝叶斯 --
+    #贝叶斯
     count_vect = CountVectorizer()
     X_train_counts = count_vect.fit_transform(train_word_list)
     X_test_counts = count_vect.transform(test_word_list)
@@ -176,7 +191,7 @@ if __name__ == '__main__':
     
     print("--------*****---------")
 
-    #SVM --
+    #SVM
     svm_clf = Pipeline([
                        ('vect',CountVectorizer()),
                         ('tfidf', TfidfTransformer()),
@@ -188,10 +203,43 @@ if __name__ == '__main__':
     predicted = svm_clf.predict(test_word_list)
     print("SVM_Avg_Precision:",np.mean(predicted == test_class_list))
 
-    for index, value in enumerate(predicted):
+    '''for index, value in enumerate(predicted):
         if value != test_class_list[index]:
-            print(index, "predicted:", value, "actual: ", test_class_list[index], "file: ", test_word_file_name_list[index])
+            print(index, "predicted:", value, "actual: ", test_class_list[index], "file: ", test_word_file_name_list[index])'''
 
     print(classification_report(test_class_list, predicted, target_names = CONTENT_TYPE.values()))
     print("SVM confusion matrix:")
     print(metrics.confusion_matrix(test_class_list, predicted))
+
+    print("--------*****---------")
+    
+    #SVM - corss-validatin
+    pipeline = Pipeline([
+                         ('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', SGDClassifier()),
+                         ])
+    
+    parameters = {
+    'vect__max_df': (0.5, 0.75, 1.0),
+    # 'vect__max_features': (None, 5000, 10000, 50000),
+    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+    # 'tfidf__use_idf': (True, False),
+    # 'tfidf__norm': ('l1', 'l2'),
+    'clf__max_iter': (5,),
+    'clf__alpha': (0.00001, 0.000001),
+    'clf__penalty': ('l2', 'elasticnet'),
+    # 'clf__max_iter': (10, 50, 80),
+    }
+    grid_search = GridSearchCV(pipeline, parameters, cv=5,
+                               n_jobs=-1, verbose=1)
+    grid_search.fit(train_word_list, train_class_list)
+    
+    #svm_rbf = svm_cross_validation(train_word_list, train_class_list)
+    
+    predicted = grid_search.predict(test_word_list)
+    print("SVM_grid_search_Precision:",np.mean(predicted == test_class_list))
+    print(classification_report(test_class_list, predicted, target_names = CONTENT_TYPE.values()))
+    print("SVM_grid_search confusion matrix:")
+    print(metrics.confusion_matrix(test_class_list, predicted))
+
